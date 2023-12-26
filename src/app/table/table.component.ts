08f44fd1583f57f21c3aedefb7cb25a86228e667
jaime <jaimerecovery23@gmail.com>
2023-12-26 22:53:01 +0100

    Added table v1

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {TeamService} from "../services/teamService/team.service";
+import {catchError, tap} from "rxjs/operators";
+import {of} from "rxjs";
 
 @Component({
   selector: 'app-tabulka',
@@ -8,6 +11,38 @@ import { Component } from '@angular/core';
 export class TableComponent {
 
   selectedLeague: string = "1.KL Západ";
+
+  teams : any [] = [];
+
+  constructor(private teamService: TeamService, private cdRef: ChangeDetectorRef) {
+  }
+
+  ngOnInit(): void {
+    this.getAllTeams()
+  }
+
+
+  getAllTeams() {
+    this.teamService.getAllTeams().pipe(
+      tap((resp: any) => {
+        console.log(resp);
+        this.teams = resp;
+        this.detectChanges();
+      }),
+      catchError((err) => {
+        console.log(err);
+        return of(null);
+      })
+    ).subscribe();
+  }
+
+  private detectChanges(): void {
+    try {
+      this.cdRef.detectChanges();
+    } catch (e) {}
+  }
+
+
 
   changeTable(league: string) {
     this.selectedLeague = league;
