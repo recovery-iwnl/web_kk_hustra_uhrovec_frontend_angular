193cbb738041e5b373de39972547be086b35c335
jaime <jaimerecovery23@gmail.com>
2024-01-15 23:53:57 +0100

    Added option to upload images to gallery

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
+import {FileUploadService} from "../services/fileUploadService/file-upload.service";
+import {catchError, tap} from "rxjs/operators";
+import {of} from "rxjs";
 
 @Component({
   selector: 'app-gallery',
@@ -6,5 +11,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./gallery.component.css']
 })
 export class GalleryComponent {
+
+
+
+  images: any[] = [];
+
+  file: File | undefined;
+
+  constructor(private http: HttpClient, private fileUploadService: FileUploadService) { }
+
+  ngOnInit() {
+    this.loadAllImages();
+  }
+
+  selectFile(event: any) {
+    this.file = event.target.files[0];
+  }
+
+  uploadFile() {
+    if (this.file) {
+      this.fileUploadService.uploadFile(this.file).subscribe(
+        (response: any) => {
+          console.log('File uploaded successfully', response);
+          this.images.push(response);
+        },
+        (error: any) => {
+          console.error('Error uploading file', error);
+        }
+      );
+    }
+
+  }
+
+  loadAllImages() {
+    this.fileUploadService.getAllImages().subscribe(
+      (response: any[]) => {
+        this.images = response;
+      },
+      (error: any) => {
+        console.error('Error loading images', error);
+      }
+    );
+  }
+
 
 }
---
+++ src/app/services/fileUploadService/file-upload.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileUploadService {
+
+  constructor(private http : HttpClient) { }
+
+  API = "http://localhost:8080";
+
+  public uploadFile(file: File) {
+    const formData: FormData = new FormData();
+    formData.append('file', file, file.name);
+    return this.http.post(this.API + '/api/v1/image/upload', formData);
+  }
+
+  public getAllImages() {
+    return this.http.get<any[]>(this.API + '/api/v1/image/all');
+  }
+
+
+}
